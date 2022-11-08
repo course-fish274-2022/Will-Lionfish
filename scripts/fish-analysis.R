bdbd8061c851eb594675b6d86ba154d55ecd4622
Will Patrone <will0926@uw.edu>
2022-11-08 10:39:35 -0800

    Added a comment above the creation of the length category column

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -4,6 +4,7 @@ library(dplyr)
 
 fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
 
+#Add a column describing the lenght of an individual as 'big' or 'small'
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
