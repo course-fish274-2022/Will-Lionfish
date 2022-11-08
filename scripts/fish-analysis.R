a2a938c47d166f018a051d9146ef8f3d49e871c8
Will Patrone <will0926@uw.edu>
2022-11-08 10:53:36 -0800

    Renamed original raw dataset with "_1". Changed code to reflect this change when reading in the .csv file

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 
-fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
+fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv")
 
 #Add a column describing the lenght of an individual as 'big' or 'small
 fish_data_cat <- fish_data %>%
