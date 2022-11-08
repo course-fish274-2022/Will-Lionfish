547aac5f00a394320216023419b792b8b90888a3
Will Patrone <will0926@uw.edu>
2022-11-08 10:48:32 -0800

    Removed data points that had a scale length > 1 due to a measurement error. I also added a comment to describe this work

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -4,7 +4,13 @@ library(dplyr)
 
 fish_data <- read.csv("Gaeta_etal_CLC_data.csv")
 
-#Add a column describing the lenght of an individual as 'big' or 'small'
+#Add a column describing the lenght of an individual as 'big' or 'small
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+#Remove fish with a scalelenght less than 1 due to an error. Could also have 
+  #added it to the fish_data_cat line of code
+fish_data_cat_fixed <-  fish_data_cat %>% 
+  filter(scalelength > 1)
+
 
