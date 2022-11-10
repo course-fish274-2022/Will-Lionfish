b2885cf20f5d4b72b0f52741179a52420da36be6
Will Patrone <will0926@uw.edu>
2022-11-10 09:39:47 -0800

    Added ggplot to the library of packages to load
    Edited the original data code to ensure it read into the correct file, as it was in the /data subdirectory
    
    Fixed a typo in a comment
    
    Commented out the code that created a new variable due to the 1mm error
    
    Created a scatterplot of fish length against scale length, with the lakeid as the color on the plot

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -1,19 +1,26 @@
 ##Load Packages##
 library(dplyr)
+library(ggplot2)
 
 
-fish_data <- read.csv("Gaeta_etal_CLC_data_1.csv")
+fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 
 #Add a column describing the lenght of an individual as 'big' or 'small
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
-#Remove fish with a scalelenght less than 1 due to an error. Could also have 
+#Remove fish with a scalelength less than 1 due to an error. Could also have 
   #added it to the fish_data_cat line of code
-fish_data_cat_fixed <-  fish_data_cat %>% 
-  filter(scalelength > 1)
+#fish_data_cat_fixed <-  fish_data_cat %>% 
+ # filter(scalelength > 1)
 
 #Alternative way of doing it
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength > 1)
+
+
+#Exercise 5
+ggplot(data = fish_data_cat)+
+  aes(x = length, y = scalelength, color = lakeid) +
+  geom_point()
