96c4507bfb16292e72af8624376a32db260a7818
Will Patrone <will0926@uw.edu>
2022-11-10 10:11:01 -0800

    Uploaded both raw data .csv files into the project and assigned them to variables in the code

--- scripts/Lionfish_Project.R
+++ scripts/Lionfish_Project.R
@@ -5,3 +5,9 @@
 
 #Load packages
 library(tidyverse)
+
+
+#Reading in raw data
+
+All_Surveys <- read.csv("data/REEF_AllSurveyData_FISH274.csv")
+Lionfish_Surveys <- read.csv("data/REEF_OnlyLionfishData_FISH274.csv")
