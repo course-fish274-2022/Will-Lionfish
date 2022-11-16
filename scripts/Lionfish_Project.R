069f97de7f800fbc42bfeb812d651c3cd2e7b4b2
Will Patrone <will0926@uw.edu>
2022-11-16 14:11:27 -0800

    Added ability to install tidyverse if needed
    Got rid of the unused csv file and the code that would read it in

--- scripts/Lionfish_Project.R
+++ scripts/Lionfish_Project.R
@@ -2,12 +2,12 @@
 #FISH 274 Project
 #Autumn 2022
 
+#Install Packages
+install.packages('tidyverse')
 
 #Load packages
 library(tidyverse)
 
 
 #Reading in raw data
-
-All_Surveys <- read.csv("data/REEF_AllSurveyData_FISH274.csv")
-Lionfish_Surveys <- read.csv("data/REEF_OnlyLionfishData_FISH274.csv")
+Surveys <- read.csv("data/REEF_AllSurveyData_FISH274.csv")
