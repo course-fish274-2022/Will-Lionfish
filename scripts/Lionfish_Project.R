6709d968bc506fa5dff3102462570525472f44ab
Will Patrone <will0926@uw.edu>
2022-11-16 21:45:55 -0800

    Ensured all column references matched new column names
    Created a plot of abundance across each year, with a linear model and 95% CI around it

--- scripts/Lionfish_Project.R
+++ scripts/Lionfish_Project.R
@@ -14,7 +14,17 @@ surveys <- read.csv("data/REEF_AllSurveyData_FISH274.csv")
 
 #Filter data, group all data by the year of the survey
 year_df <- surveys %>%
-  group_by(Survey_year) %>% 
-  summarize(yearly_avg_abundance = mean(Abundance2))
+  group_by(year) %>%
+  filter(year >= "2009") %>% 
+  summarize(yearly_avg_abundance = mean(abundance))
 
-#Map Changes in abundance by year            
+#Graph Changes in abundance by year  
+year_df %>% 
+  ggplot(aes(x = year, y = yearly_avg_abundance))+
+  geom_point()+
+  theme_bw()+
+  scale_x_log10()+
+  scale_y_log10()+
+  labs(title = "Yearly Abundance of Lionfish in the Flordia Keys", y = "Year", x = "Relative Abundance of Lionfish")+
+  geom_smooth(method = lm)
+
